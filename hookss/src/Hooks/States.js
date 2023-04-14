cb1c99037cdc5994421033ae7a508f3e38db98f7
dharan-dhakshan97 <dharanidhakshan97@gmail.com>
2023-04-14 19:50:33 +0530

    add initial commit

--- hookss/src/Hooks/States.js
+++ hookss/src/Hooks/States.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React from 'react';
+import {useState} from 'react';
 
 const States = () => {
+
+const [incre,setIncre]=useState(0);
+
+function increment()
+{
+  setIncre(incre+1);
+}
+
+
+const [decre,setDecre]=useState(1);
+function decrement()
+{
+  setDecre(decre-1);
+}
+
+
   return (
-    <div>States</div>
+    <>
+
+    <h1>The Increment Value is : {incre}</h1>
+    <button onClick={increment}>Increment</button>
+
+
+<h1>The Decrement Value is:{decre}</h1>
+<button onClick={decrement}>Decrement</button>
+
+
+
+
+
+
+    </>
   )
 }
 
